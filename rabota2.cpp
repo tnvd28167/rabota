029ae7a380ab64d2f8f4aacdf8658cb871e817a0
tnvd28167 <minasyantigran2007@gmail.com>
2025-09-16 10:44:51 +0300

    Create rabota2.cpp

---
+++ rabota2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    long long A, B;
+    std::cout << "enter 2 numbers separated by a space: ";
+    std::cin >> A >> B;
+    std::cout << "summa: " << A + B << " ";
+    std::cout << "vichetanie: " << A - B << " ";
+    std::cout << "ymnojenie: " << A * B << " ";
+    std::cout << "delenie: " << A / B << " ";
+    return 0;
+}
